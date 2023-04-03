117a8b8b161f7b9a785bac54e8f17cd1c72b79c1
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-04-03 20:35:46 +0300

    Data Structures Modern Operators and Strings

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ const restaurant = {
     }
 };
 
+/*
 const question = new Map([
     ['question', 'What is the best programming language in the world ?'],
     [1, 'C'],
@@ -85,10 +86,12 @@ console.log(...question)
     // console.log(question.entries())
 console.log([...question.keys()])
 console.log([...question.values()])
+*/
+
 
 /*
 //* MAPS : Fundementals
-// MAP is a data structure that we can use to map values to keys. So, just like an object data is stored in key value pairs in maps.
+//* MAP is a data structure that we can use to map values to keys. So, just like an object data is stored in key value pairs in maps.
 
 const rest = new Map()
 rest.set('name', 'Classico Italiano') // first element in the array 
@@ -115,7 +118,7 @@ const arr = [1, 2]
 rest.set(arr, 'Test') // add a new key to the original array.
 rest.set(document.querySelector('h1'), 'Heading')
 console.log(rest)
-console.log(rest.size) // the conse shows the length of the array
+console.log(rest.size) // the console shows the length of the array
 
 console.log(rest.get(arr)) // the console shows the arr key
 */
@@ -472,3 +475,46 @@ for (const [team, odd] of Object.entries(game.odds)) {
     console.log(`Odd of ${teamStr} ${odd}`)
 }
 */
+
+
+/*
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+
+//* TK - 3
+const gameEvents = new Map([
+    [17, '⚽️ GOAL'],
+    [36, '🔁 Substitution'],
+    [47, '⚽️ GOAL'],
+    [61, '🔁 Substitution'],
+    [64, '🔶 Yellow card'],
+    [69, '🔴 Red card'],
+    [70, '🔁 Substitution'],
+    [72, '🔁 Substitution'],
+    [76, '⚽️ GOAL'],
+    [80, '⚽️ GOAL'],
+    [92, '🔶 Yellow card'],
+]);
+
+// 1)
+const events = [...new Set(gameEvents.values())]
+console.log(events)
+
+// 2)
+gameEvents.delete(64)
+console.log(gameEvents)
+
+// 3)
+console.log(`An event happened, on average, every ${90 / gameEvents.size} minutes`)
+
+// 4)
+for (const [key, value] of gameEvents) {
+    (typeof key === 'number' && key < 45 ? console.log(`[first half] ${key}:${value}`) : console.log(`[second half] ${key}:${value}`))
+}
