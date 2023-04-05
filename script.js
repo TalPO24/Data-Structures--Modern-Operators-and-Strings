ee947e850370f15b4dc180cb9b7caecf275397c4
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-04-05 14:01:54 +0300

    Data Structures Modern Operators and Strings

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 // Data needed for first part of the section
 const weekdays = ['mon', 'tue', 'wen', 'thu', 'fri', 'sat', 'sun']
 const hours = {
@@ -681,15 +677,14 @@ for (const [key, value] of gameEvents) {
 }
 */
 
-
-//* TK-4
 /*
+//* TK-4
 underscore_case
 first_name
 Some_Variable
 Calculate_AGE
 delayed_departure
-*/
+
 
 document.body.append(document.createElement('textarea'))
 document.body.append(document.createElement('button'))
@@ -706,3 +701,17 @@ document.querySelector('button').addEventListener('click', function() {
         console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`)
     }
 })
+*/
+
+
+//* TK-5
+const flights =
+    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase()
+
+for (const flight of flights.split("+")) {
+    const [type, from, to, time] = (flight.split(";"))
+    const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll("_", ' ')} ${getCode(from)} ${getCode(to)} (${time.replace(':','h')})`.padStart(36)
+    console.log(output)
+}
